34beacaafeb6e96906422899bc1cacee55567ca7
Fred Garcia <Fred@Garcia.com>
2020-12-03 14:52:15 -0800

    .

--- src/Nav.js
+++ src/Nav.js
@@ -36,11 +36,11 @@ function Nav() {
             </Link>
             
             <Link to={"/login"} style={{textDecoration:'none'}}>
-                <div className="nav__signIn" onClick={login}>
+                <div className={!user? 'nav__signIn' : 'nav__avatar'} onClick={login}>
                     <img src={user? 'https://pro2-bar-s3-cdn-cf1.myportfolio.com/dddb0c1b4ab622854dd81280840458d3/98032aebff601c1d993e12a0_rw_600.png?h=8030f4d5734548795c22da59ca72e3e1' : ''} 
                      className="nav__avatar"
                     alt=""/>
-                     {user? 'Sign out': 'Sign in'}
+                   {!user? 'Sign in ' : ''}
                 </div>
             </Link>
            
